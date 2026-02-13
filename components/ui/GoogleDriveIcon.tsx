e783110b28913c680c0f52d69027d953f481a5d2
Marawan <marawandeep13@gmail.com>
2026-02-13 17:43:15 +0100

    Change Google Drive icon colors to green

--- components/ui/GoogleDriveIcon.tsx
+++ components/ui/GoogleDriveIcon.tsx
@@ -16,7 +16,7 @@ export default function GoogleDriveIcon({ size = 24, className = '' }: GoogleDri
     >
       <path
         d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z"
-        fill="#0066da"
+        fill="#00ac47"
       />
       <path
         d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0 -1.2 4.5h27.5z"
@@ -32,7 +32,7 @@ export default function GoogleDriveIcon({ size = 24, className = '' }: GoogleDri
       />
       <path
         d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z"
-        fill="#2684fc"
+        fill="#00ac47"
       />
       <path
         d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z"
